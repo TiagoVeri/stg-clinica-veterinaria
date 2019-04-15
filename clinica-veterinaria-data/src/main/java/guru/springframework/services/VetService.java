34add15affd7c3d03b134ba3451fb5a440a90918
TiagoVeri <tiagoverisjc@gmail.com>
2019-04-15 15:39:46 -0300

    Added Vet List and updated controller. Closes #16

--- clinica-veterinaria-data/src/main/java/guru/springframework/services/VetService.java
+++ clinica-veterinaria-data/src/main/java/guru/springframework/services/VetService.java
@@ -1,7 +1,5 @@
 package guru.springframework.services;
 
-import java.util.Set;
-
 import guru.springframework.model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
--- clinica-veterinaria-web/src/main/java/guru/springframework/controllers/VetController.java
+++ clinica-veterinaria-web/src/main/java/guru/springframework/controllers/VetController.java
@@ -1,13 +1,26 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.services.VetService;
+
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 	
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
-		return "vets/index";
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"", "/", "/index", "index.html"})
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
+		return "vets/index"; 
 	}
 }
